3d726c30f4c2bb457543ad86e9231a1750ddd291
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2025-11-17 22:14:42 +0100

    Fin

--- welcome.sh
+++ welcome.sh
@@ -1,1 +1,1 @@
-echo -e "\033[36m Welcome, you've made \033[35m $(git rev-list --count HEAD) \033[36m commits \033[0m"
+echo -e "\033[36m Welcome, you've made \033[35m $(git rev-list --count HEAD) \033[36m commits! \033[0m"
