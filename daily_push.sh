434ccc73fe895e135eff159d2e42e404d26eec49
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2025-11-17 20:12:41 +0100

    lun. 17 nov. 2025 20:12:41 CET

---
+++ daily_push.sh
@@ -0,0 +1,5 @@
+cd /home/dawid/Pool/pool-day-01
+
+git add .
+git commit -m "$(date)"
+git push 
---
+++ ex_06.sh
@@ -0,0 +1,6 @@
+touch log.txt
+git log >> log.txt
+
+git add log.txt
+git commit -m "Ex_06"
+git push 
