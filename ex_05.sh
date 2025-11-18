b24c404c4fdf00babf423009b748fb8ece0e1fee
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2025-11-18 09:52:33 +0100

    mar. 18 nov. 2025 09:52:33 CET

--- ex_05.sh
+++ ex_05.sh
@@ -1,6 +1,9 @@
 touch readme.md
 date >> readme.md
 
+git config -- global user . name " Dawid Wieclaw "
+git config -- global user . email " dawid.wieclaw@epitech.eu "
+
 git add .
 git commit -m "Ex_05"
 git push
